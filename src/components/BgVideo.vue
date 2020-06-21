a9240f01a149b90634a4ea24d72cafcf1d938e7c
Gopi <haigopi@gmail.com>
2020-06-21 18:08:33 -0500

    Update BgVideo.vue

--- src/components/BgVideo.vue
+++ src/components/BgVideo.vue
@@ -3,7 +3,7 @@
     <VideoBg
       src="https://vsassets.netlify.app/vantashala.mp4"
       style="height: 100vh;"
-      class= "green lighten-4"
+      class="green lighten-4"
       v-bind:overlay="countryChanged"
     >
       <ToolBar style="position: absolute" />
@@ -25,14 +25,12 @@ export default Vue.extend({
     ToolBar,
     Carousel
   },
-computed: {
+  computed: {
     countryChanged: {
       get() {
-        
-          const country = this.$store.getters.getCountry;
-          const styles = this.visuals[country];
-          return styles["overlay"];
-       
+        const country = this.$store.getters.getCountry;
+        const styles = this.visuals[country];
+        return styles["overlay"];
       }
     }
   },
@@ -42,10 +40,10 @@ computed: {
         overlay: "linear-gradient(45deg,#2a4ae4B3,#fb949eB3)"
       },
       USA: {
-        overlay: "linear-gradient(45deg,#984dddB3,#fb949eB3)"
+        overlay: "linear-gradient(45deg,#aed581B3,#b3e5fcB3)"
       },
       SINGAPORE: {
-        overlay: "linear-gradient(45deg,#764bb4B3,#fb949eB3)"
+        overlay: "linear-gradient(45deg,#ffcc80B3,#fb949eB3)"
       }
     }
   })
