295bd4fbac71b66cd2ef47dbff771f54778e9445
Gopi <haigopi@gmail.com>
2020-06-22 17:57:31 -0500

    Responsive

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,21 +3,12 @@
     <v-carousel hide-delimiters continuous show-arrows-on-hover cycle>
       <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
         <v-container fill-height fluid ma-0 pa-0>
-          <v-card class="transparent card_mgleft" outlined>
+          <v-card class="transparent card_mgleft" outlined xs12 sm6 offset-sm3>
             <v-list-item>
               <v-list-item-content>
-                <div class="overline mb-10 font-weight-bold">
-                  {{ item.caption }}
-                </div>
-                <v-list-item-title class="text-h2 font-weight-bold mb-5">
-                  {{ item.title }}
-                </v-list-item-title>
-                <v-list-item-subtitle
-                  class="text-subtitle-1 font-weight-bold"
-                  mb-2
-                >
-                  {{ item.desc }}
-                </v-list-item-subtitle>
+                <div class="overline mb-10 font-weight-bold">{{ item.caption }}</div>
+                <v-list-item-title class="text-h2 font-weight-bold mb-5">{{ item.title }}</v-list-item-title>
+                <v-list-item-subtitle class="text-subtitle-1 font-weight-bold" mb-2>{{ item.desc }}</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
           </v-card>
--- src/components/toolbar/ToolBar.vue
+++ src/components/toolbar/ToolBar.vue
@@ -1,16 +1,15 @@
 <template>
   <v-container fluid>
-    <v-toolbar
+    <v-app-bar
       flat
       dense
       elevate-on-scroll
       v-bind:style="styles"
       text-center
       align="center"
+      clipped-right
     >
-      <v-toolbar-title class="ma-2 text-h6 white--text font-weight-bold"
-        >VantaShala</v-toolbar-title
-      >
+      <v-toolbar-title class="ma-2 text-h6 white--text font-weight-bold">VantaShala</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn
@@ -20,27 +19,61 @@
           text
           small
           class="ma-auto white--text font-weight-bold"
-          >{{ item.title }}</v-btn
-        >
+        >{{ item.title }}</v-btn>
 
         <div class="dropdown ma-auto">
-          <select
-            name="country"
-            @change="onchange()"
-            class="dropdown-select"
-            v-model="key"
-          >
+          <select name="country" @change="onchange()" class="dropdown-select ma-auto" v-model="key">
             <option
               default
               v-for="country in countries"
               :key="country.name"
               :value="country.value"
-              >{{ country.name }}</option
-            >
+            >{{ country.name }}</option>
           </select>
         </div>
       </v-toolbar-items>
-    </v-toolbar>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary absolute width="200" id="drawer" color="green">
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item v-for="item in menu" :key="item.icon">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+
+            <v-btn
+              :to="item.link"
+              text
+              small
+              class="ma-auto white--text font-weight-bold"
+            >{{ item.title }}</v-btn>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <div class="dropdown ma-auto">
+              <select
+                name="country"
+                @change="onchange()"
+                class="dropdown-select ma-auto"
+                v-model="key"
+              >
+                <option
+                  default
+                  v-for="country in countries"
+                  :key="country.name"
+                  :value="country.value"
+                >{{ country.name }}</option>
+              </select>
+            </div>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-container>
 </template>
 
@@ -51,6 +84,8 @@ export default Vue.extend({
   name: "ToolBar",
 
   data: () => ({
+    drawer: false,
+    key: "INDIA",
     menu: [
       { icon: "home", title: "My Orders" },
       { icon: "info", title: "My Chefs" },
@@ -71,7 +106,6 @@ export default Vue.extend({
         value: "USA"
       }
     ],
-    key: "INDIA",
 
     styles: {
       "background-color": `transparent`
