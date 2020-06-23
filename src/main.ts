2b6c9ddab0a178df57295e232bd1e869d283d932
Gopi <haigopi@gmail.com>
2020-06-22 21:41:32 -0500

    Addin Vue Meta

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VideoBackground from "vue-responsive-video-background-player";
+import VueMeta from 'vue-meta'
+
+Vue.use(VueMeta)
 Vue.component("VideoBg", VideoBackground);
 Vue.config.productionTip = false;
 
