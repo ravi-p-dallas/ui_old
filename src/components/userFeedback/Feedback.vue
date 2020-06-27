702cdd2aaee4d39726f1abd673e11f7dea7d6798
Gopi <haigopi@gmail.com>
2020-06-27 15:49:40 -0500

    Update Feedback.vue

--- src/components/userFeedback/Feedback.vue
+++ src/components/userFeedback/Feedback.vue
@@ -1,84 +1,38 @@
-
 <template>
-  <v-container fluid v-scroll="onScroll">
-    <v-row align="center" justify="center" no-gutters>
-      <v-col lg="2" class="ma-2" v-for="item in items" :key="item.Char">
-        <FeedbackCard
-          :Title="item.title"
-          :Char="item.char"
-          :Caption="item.caption"
-          :CustomStyle="item.customStyle"
-          :scrollId="item.scrollId"
-        />
-        <v-btn v-show="fab" fab fixed bottom right color="primary" @click="toTop">
-          <v-icon>mdi-chevron-down</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-card fluid class="mx-auto ma-10" flat >
+    <v-list-item class="ma-0 pa-0">
+      <v-list-item-content class="ma-0 pa-0">
+        <v-list-item-title class="text-center headline mx-auto font-weight-bold">
+          <div style="letter-spacing:6px">WHAT OUR FANTASTIC USERS SAY</div>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-text class="ma-0 pa-0">
+      <v-timeline>
+        <v-timeline-item v-for="n in 4" :key="n" large>
+          <template v-slot:icon>
+            <v-avatar>
+              <img src="http://i.pravatar.cc/64" />
+            </v-avatar>
+          </template>
+          <template v-slot:opposite>
+            <span>Gopi Kancharala</span>
+          </template>
+          <v-card class="elevation-2">
+            <v-card-text>“Didn't expect this level of service. Very nice and continue it.”</v-card-text>
+          </v-card>
+        </v-timeline-item>
+        <div class="ma-auto text-center">
+          <v-btn color="green lighten-3 mr-1 white--text" small>
+            <v-icon small>mdi-arrow-left</v-icon>
+          </v-btn>
+          
+          <v-btn color="orange lighten-3 green--text" small>
+            <v-icon small>mdi-arrow-right</v-icon>
+          </v-btn>
+        </div>
+      </v-timeline>
+    </v-card-text>
+  </v-card>
 </template>
-
-<style scoped>
-.bg {
-  background: rgb(255, 255, 255);
-  
-  background: linear-gradient(0deg, rgba(255, 255, 255, 1) 0%, rgb(255, 248, 225, 1) 100%);
-}
-</style>
-
-<script lang="ts">
-import Vue from "vue";
-import FeedbackCard from "./FeedbackCard.vue";
-
-export default Vue.extend({
-  name: "ActionButtons",
-
-  components: {
-    FeedbackCard
-  },
-  data: () => ({
-    fab: false,
-    items: [
-      {
-        title: "BiWeekly Orders",
-        char: "W",
-        caption: "Delivered on bi-weekly basis",
-        customStyle: "green lighten-3",
-        scrollId: "biWeekly_id"
-      },
-      {
-        title: "Monthly Orders",
-        char: "B",
-        caption: "Delivered on bi-weekly basis",
-        customStyle: "orange lighten-3",
-        scrollId: "monthly_id"
-      },
-      {
-        title: "Weekly Orders",
-        char: "M",
-        caption: "Delivered on bi-weekly basis",
-        customStyle: "red lighten-3 white--text",
-        scrollId: "weekly_id"
-      },
-      {
-        title: "Daily Orders",
-        char: "D",
-        caption: "Delivered on bi-weekly basis",
-        customStyle: "blue lighten-3",
-        scrollId: "daily_id"
-      }
-    ]
-  }),
-  methods: {
-    onScroll(e) {
-      console.log(typeof window);
-      if (typeof window === "undefined") return;
-      const top = window.pageYOffset || e.target.scrollTop || 0;
-      this.fab = top > 60;
-    },
-    toTop() {
-      this.$vuetify.goTo(0);
-    }
-  }
-});
-</script>
