c0db69551c49b3ef2e1a546de3a831667f46d66d
Gopi <haigopi@gmail.com>
2020-07-01 17:37:50 -0500

    Restructure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <!-- d-flex flex-column -->
-      <v-container fluid fill-height class="ma-0 pa-0 d-flex flex-column">
-        <ToolBar scroll-target="#scrolling-techniques-2" hide-on-scroll prominent />
-        <BgVideo />
-        <v-btn v-show="fab" fab fixed bottom right color="primary" @click="toTop">
-          <v-icon>mdi-chevron-up</v-icon>
-        </v-btn>
-        <RepeatedOrders id="scrolling-techniques-5" class="mb-0 grey lighten-5" />
-        <ContactUs />
-        <Feedback />
-        <Footer />
-      </v-container>
+      <HomeView />
     </v-main>
   </v-app>
 </template>
@@ -21,38 +10,15 @@
 
 <script lang="ts">
 import Vue from "vue";
-import ToolBar from "./components/homeTop/toolbar/ToolBar.vue";
-import BgVideo from "./components/homeTop/BgVideo.vue";
-import RepeatedOrders from "./components/homeNext/RepeatedOrders.vue";
-import ContactUs from "./components/conatctUs/ContactUs.vue";
-import Feedback from "./components/userFeedback/Feedback.vue";
-import Footer from "./components/footer/Footer.vue";
+import HomeView from "./views/Home.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    ToolBar,
-    BgVideo,
-    RepeatedOrders,
-    ContactUs,
-    Feedback,
-    Footer
+    HomeView
   },
 
-  data: () => ({ fab: false }),
-
-  methods: {
-    onScroll(e) {
-      console.log(typeof window);
-      if (typeof window === "undefined") return;
-      const top = window.pageYOffset || e.target.scrollTop || 0;
-      this.fab = top > 60;
-    },
-    toTop() {
-      this.$vuetify.goTo(0);
-    }
-  },
   metaInfo: {
     title: "VantaShala",
     titleTemplate: "%s | My Organic Chef",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,65 @@
 <template>
-  <div class="home">
+  <!-- d-flex flex-column -->
+  <v-container fluid fill-height class="ma-0 pa-0 d-flex flex-column">
+    <ToolBar scroll-target="#scrolling-techniques-2" hide-on-scroll prominent />
     <BgVideo />
-  </div>
+    <v-btn v-show="fab" fab fixed bottom right color="primary" @click="toTop">
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+    <RepeatedOrders id="scrolling-techniques-5" class="mb-0 grey lighten-5" />
+    <ContactUs />
+    <Feedback />
+    <Footer />
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import BgVideo from "@/components/homeTop/BgVideo.vue";
+<style scoped></style>
 
-export default {
-  name: "Home",
+<script lang="ts">
+import Vue from "vue";
+import ToolBar from "../components/home/toolbar/ToolBar.vue";
+import BgVideo from "../components/home/BgVideo.vue";
+import RepeatedOrders from "../components/homeNext/RepeatedOrders.vue";
+import ContactUs from "../components/conatctUs/ContactUs.vue";
+import Feedback from "../components/userFeedback/Feedback.vue";
+import Footer from "../components/footer/Footer.vue";
+
+export default Vue.extend({
+  name: "App",
+
   components: {
-    BgVideo
+    ToolBar,
+    BgVideo,
+    RepeatedOrders,
+    ContactUs,
+    Feedback,
+    Footer
+  },
+
+  data: () => ({ fab: false }),
+
+  methods: {
+    onScroll(e) {
+      console.log(typeof window);
+      if (typeof window === "undefined") return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 60;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    }
+  },
+  metaInfo: {
+    title: "VantaShala",
+    titleTemplate: "%s | My Organic Chef",
+    link: [{ rel: "favicon", href: "favicon.ico" }],
+    meta: [
+      {
+        vmid: "Organaic Chef",
+        name: "Clean Healthy Home Cooked Food",
+        content: "Home food at your door"
+      }
+    ]
   }
-};
+});
 </script>
