17701b5a0423c08c09e2f2fe93c54778f948cb80
Gopi <haigopi@gmail.com>
2020-07-03 22:24:14 -0500

    Update LayoutSignInBeAChefCard.vue

--- src/components/howDoWeOperate/LayoutSignInBeAChefCard.vue
+++ src/components/howDoWeOperate/LayoutSignInBeAChefCard.vue
@@ -2,48 +2,29 @@
   <v-card :loading="loading" class="mx-auto">
     <v-card-title>
       <div :style="$vuetify.breakpoint.smAndDown ? 'letter-spacing:0px' : 'letter-spacing:1px'" class="text_card_style">
-        BECOME A PROUD CHEF - WORLD IS WAITING YOU
+        BECOME A PROUD CHEF - WORLD IS WAITING FOR YOUR PASSION.
       </div>
     </v-card-title>
     <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
 
     <v-card-text>
-      <v-row align="center" class="mx-0">
-        <v-rating :value="4.5" color="amber" dense half-increments readonly size="14"></v-rating>
-
-        <div class="grey--text ml-4">4.5 (413)</div>
-      </v-row>
-
       <div class="my-4 subtitle-1">
-        $ • Italian, Cafe
+        VantaShala has Protocols for both Customers & Chefs.
       </div>
 
       <div>
-        Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.
+        Follow the CHEF Guide Lines.
       </div>
     </v-card-text>
 
     <v-divider class="mx-4"></v-divider>
 
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group v-model="selection" active-class="deep-purple accent-4 white--text" column>
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-actions>
+    <v-card-title ma-auto>
+      <v-text-field v-model="message4" label="Name your Kitchen:" outlined clearable></v-text-field>
       <v-btn color="deep-purple lighten-2" text @click="reserve">
         Reserve
-      </v-btn>
-    </v-card-actions>
+      </v-btn></v-card-title
+    >
   </v-card>
 </template>
 
