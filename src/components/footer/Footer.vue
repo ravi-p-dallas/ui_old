d6811fd495ec3956b2d9561da2a5d3d8650ebc7d
Guntupalli Koushik Manikanta <31413140+guntupallikoushik1999@users.noreply.github.com>
2020-07-20 13:09:52 +0530

    Update Footer.vue

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,18 +1,37 @@
 <template>
   <v-container fluid ma-0 pa-0>
     <v-footer dark padless>
-      <v-card flat tile class="grey-blue lighten-1 white--text text-center" style="width:100%">
+      <v-card
+        flat
+        tile
+        class="grey-blue lighten-1 white--text text-center"
+        style="width:100%"
+      >
         <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
             <v-icon size="24px">{{ icon }}</v-icon>
           </v-btn>
         </v-card-text>
 
         <v-card-text class="white--text pt-0">
+          Gopi to Work on this
           <pre>
 ASIA
 Beside CPM Office, Raitupeta
 Nandigama, India, 521185
+
+USA
+2700 Main St, #120
+Frisco, TX, USA, 75034
+
+UK
+Kemp House, 152 - 160 City Road,
+London EC1V 2NX
 </pre
           >
         </v-card-text>
