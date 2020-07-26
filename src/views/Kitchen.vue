52950535ce9a54f8374773429a9ad8901d7552f2
IndrajaChelimela <chelimelaindraja1111@gmail.com>
2020-07-26 23:12:59 +0530

    footer changes
    second commit

--- src/views/Kitchen.vue
+++ src/views/Kitchen.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid fill-height class="ma-0 pa-0 d-flex flex-column" scroll-y>
     <ToolBar> </ToolBar>
-    <LegalLink class="mt-15" />
+    <KitchenLink class="mt-15" />
     <Footer id="FT" />
   </v-container>
 </template>
@@ -13,7 +13,7 @@ import Vue from "vue";
 import ToolBar from "../components/home/toolbar/ToolBar.vue";
 
 import Footer from "../components/footer/Footer.vue";
-import LegalLink from "../components/footer/links/LegalLink.vue";
+import KitchenLink from "../components/footer/links/KitchenLink.vue";
 
 export default Vue.extend({
   name: "App",
@@ -21,7 +21,7 @@ export default Vue.extend({
   components: {
     ToolBar,
     Footer,
-    LegalLink
+    KitchenLink
   }
 });
 </script>
