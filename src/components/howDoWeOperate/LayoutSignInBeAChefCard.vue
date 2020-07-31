e135863466b2f20901aa0b304967566b6402f38b
Indraja Chelimela <54662327+IndrajaChelimela@users.noreply.github.com>
2020-07-31 19:25:39 +0530

    Update LayoutSignInBeAChefCard.vue (#30)

--- src/components/howDoWeOperate/LayoutSignInBeAChefCard.vue
+++ src/components/howDoWeOperate/LayoutSignInBeAChefCard.vue
@@ -6,10 +6,10 @@
       </div>
     </v-card-title>
 
-    <v-img height="395" src="https://cdn.pixabay.com/photo/2017/10/09/19/29/eat-2834549_960_720.jpg"></v-img>
+    <v-img height="420" src="https://cdn.pixabay.com/photo/2017/10/09/19/29/eat-2834549_960_720.jpg"></v-img>
     <v-card-text>
       <v-row align="center" class="mx-0">
-        <div class="blue-grey--text ml-2">Rest assured. Strat Simple.</div>
+        <div class="blue-grey--text ml-2 mt-1">Rest assured. Strat Simple.</div>
         <v-spacer />
         <div class="blue--text">Follow the Guidelines</div>
       </v-row>
@@ -17,38 +17,83 @@
 
     <v-divider class="mx-4"></v-divider>
 
-    <v-card-text class="ma-0 pa-7">
-    <v-row align="center" class="mx-0">
+    <v-card-text>
       <v-text-field id="kitchName" name="kitchName" label="Name your Kitchen" outlined clearable></v-text-field>
-      <v-spacer />
-    <v-card-actions :class="this.$vuetify.breakpoint.smAndDown ? 'd-flex flex-column ma-5' : ''">
-      <div class="text-center">
-        <v-btn class="white--text font-weight-bold red darken-1 ">
-          <v-icon left>mdi-chef-hat</v-icon>
-          Reserve
+    </v-card-text>
+    
+    <v-card-actions :class="this.$vuetify.breakpoint.smAndDown ? 'd-flex flex-column text-right ' : ''">
+      <div class="text-center" v-for="item in items" :key="item.btnText">
+        <v-btn :class="countryChanged(item.className)" @click.stop="handleFunctionCall(item.btnText)">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.btnText }}
         </v-btn>
-        <v-btn class="white--text font-weight-bold blue darken-1 ml-4"> Existing Chef? <v-icon >mdi-key</v-icon> Login </v-btn>
       </div>
     </v-card-actions>
-    </v-row>
-    </v-card-text>
+   
   </v-card>
 </template>
 
 <style lang="scss">
 .v-text-field{
-    width: 300px;
-  }
+  height: 71px;
+}
 </style>
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
   name: "LayoutCard",
   props: [],
+  
+  computed: {
+    countryChanged: {
+      get() {
+        return className => {
+          const country = this.$store.getters.getCountry;
+          const styles = this.visuals[country];
+          return styles[className];
+        };
+      }
+    }
+  },
   data: () => ({
     loading: false,
     selection: 1,
-    image: require("@/assets/biryani_2.jpg")
+    image: require("@/assets/biryani_2.jpg"),
+    items: [
+      {
+        btnText: "RESERVE",
+        icon: "mdi-chef-hat",
+        className: "fCclass"
+      },
+
+      {
+        btnText: "EXISTING CHEF? LOGIN",
+        icon: "mdi-key",
+        className: "sMclass"
+      }
+    ],
+    visuals: {
+      INDIA: {
+        fCclass: "ma-2 white--text font-weight-bold orange darken-1",
+        sMclass: "ma-2 white--text font-weight-bold green darken-1"
+      },
+      USA: {
+        fCclass: "ma-2 white--text font-weight-bold red darken-1",
+        sMclass: "ma-2 white--text font-weight-bold blue darken-1"
+      },
+      SINGAPORE: {
+        fCclass: "ma-2 white--text font-weight-bold red lighten-1",
+        sMclass: "ma-2 red--text font-weight-bold white darken-1"
+      },
+      CANADA: {
+        fCclass: "ma-2 white--text font-weight-bold red lighten-1",
+        sMclass: "ma-2 red--text font-weight-bold white darken-1"
+      },
+      MALAYSIA: {
+        fCclass: "ma-2 white--text font-weight-bold red lighten-1",
+        sMclass: "ma-2 white--text font-weight-bold blue darken-1"
+      }
+    }
   }),
 
   methods: {
