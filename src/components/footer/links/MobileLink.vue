379f801a39de0690c63b43b4234d15c3b1467fd5
Guntupalli Koushik Manikanta <guntupallikoushik1999@github.com>
2020-08-07 08:17:46 +0530

    Update MobileLink.vue

--- src/components/footer/links/MobileLink.vue
+++ src/components/footer/links/MobileLink.vue
@@ -1,7 +1,12 @@
 <template>
   <v-container fluid grid-list-md>
-    <v-title text-align-center>Install Vantashala for better Experience</v-title>
-    <v-card max-width="344" class="mx-auto">
+    <v-card class="transparent card_mgleft" outlined xs12 sm6 offset-sm6>
+      <v-card-title class="justify-center text-center headline font-weight-bold">
+    <div :style="$vuetify.breakpoint.mobile ? 'letter-spacing:0px' : 'letter-spacing:6px'" class="text_card_style text-align-center">
+      INSTALL MOBILE APP for better experience
+    </div>
+      </v-card-title>
+    <v-card max-width="400" class="mx-auto">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="headline">For ANDROID </v-list-item-title>
@@ -10,7 +15,7 @@
 
       <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
     </v-card>
-    <v-card max-width="344" class="mx-auto">
+    <v-card max-width="400" class="mx-auto">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="headline">For IOS</v-list-item-title>
@@ -19,7 +24,7 @@
 
       <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
     </v-card>
-    <v-card max-width="500" class="mx-auto">
+    <v-card max-width="600" class="mx-auto">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="headline">For DESKTOP </v-list-item-title>
@@ -28,7 +33,7 @@
 
       <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
     </v-card>
-  </v-container>
+    </v-card>  </v-container>
 </template>
 <script>
 export default {
