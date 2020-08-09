b05fa9c13633ea616fba0dfa4a85c8db86c907ba
propardhu <parthasai88@gmail.com>
2020-08-09 11:29:46 +0530

    Positioning_Logo_Hover

--- src/components/home/toolbar/ToolBar.vue
+++ src/components/home/toolbar/ToolBar.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container fluid class="pa-0 ma-0">
     <v-app-bar :style="tbStyle" text-center align="center" class="pa-0" elevate-on-scroll fixed>
-      <v-app-bar-nav-icon>
+      <v-app-bar-nav-icon class="ml-1 pa-7">
         <router-link to="/">
-        <v-img :src="image" class="ml-8" width="48px"></v-img>
+        <v-img :src="image" width="48px"></v-img>
         </router-link>
       </v-app-bar-nav-icon>
 
-      <v-toolbar-title class="ml-2 text-h6 white--text font-weight-bold gradient-text-logo"><router-link to="/">VantaShala</router-link></v-toolbar-title>
+      <v-toolbar-title class="pl-2 text-h6 white--text font-weight-bold gradient-text-logo"><router-link to="/">VantaShala</router-link></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items v-if="updateExists" color="primary">
           <v-btn color="purple lighten-4" text small class="ma-auto white--text font-weight-bold" @click="refreshApp">
