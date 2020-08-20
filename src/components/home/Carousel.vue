ca4aef6c3c786350b331202aa3ef68042cf1e56a
IndrajaChelimela <chelimelaindraja1111@gmail.com>
2020-08-20 12:03:54 +0530

    Update Carousel.vue
    carousel text in mobile view

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -4,7 +4,7 @@
       <v-carousel hide-delimiters continuous show-arrows-on-hover cycle :interval="CarouselTime">
         <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
           <v-container fill-height fluid ma-0 pa-0>
-            <v-card class="transparent card_mgleft" outlined xs12 sm6 offset-sm3>
+            <v-card :class="$vuetify.breakpoint.smAndDown ? 'transparent carousel-item text-center' :'transparent card_mgleft'" outlined xs12 sm6 offset-sm3>
               <v-list-item>
                 <v-list-item-content>
                   <div class="overline mb-10 font-weight-bold" style="text-shadow: 1px 1px 4px rgba(0,0,0,0.25);">
