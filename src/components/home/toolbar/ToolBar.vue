e33321c32cbf495548515c22d6f05b1d0e10cb81
propardhu <parthasai88@gmail.com>
2020-08-22 19:53:30 +0530

    Update ToolBar.vue

--- src/components/home/toolbar/ToolBar.vue
+++ src/components/home/toolbar/ToolBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="pa-0 ma-0">
     <v-app-bar :style="tbStyle" text-center align="center" class="pa-0" elevate-on-scroll fixed v-scroll="onScroll">
-      <v-app-bar-nav-icon :class="this.$vuetify.breakpoint.mdAndDown ? 'pa-3' : 'ml-2 pa-7'">
+      <v-app-bar-nav-icon :class="this.$vuetify.breakpoint.mdAndDown ? 'pa-0' : 'ml-2 pb-1 '">
         <a href="/">
           <v-img :src="image" width="48px"></v-img>
         </a>
